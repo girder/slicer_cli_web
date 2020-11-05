00d204f596218698f04f448606b6686607155a74
David Manthey <david.manthey@kitware.com>
2020-11-05 14:18:02 -0500

    Add some tests.

--- tests/test_rest_slicer_cli.py
+++ tests/test_rest_slicer_cli.py
@@ -1,8 +1,11 @@
 import json
 import os
 import pytest
+from pytest_girder.assertions import assertStatusOk, assertStatus
 
 from girder.api import rest
+from girder.models.collection import Collection
+from girder.models.folder import Folder
 from girder.models.item import Item
 
 from slicer_cli_web import docker_resource
@@ -51,3 +54,68 @@ def test_genHandlerToRunDockerCLI(server, admin, folder, file):
     assert kwargs['pull_image'] == 'if-not-present'
     container_args = kwargs['container_args']
     assert container_args[0] == 'data'
+
+
+@pytest.mark.plugin('slicer_cli_web')
+def test_get_matching_resource(server, admin):
+    # Make some resources
+    publicFolder = Folder().find({'parentId': admin['_id'], 'name': 'Public'})[0]
+    privateFolder = Folder().find({'parentId': admin['_id'], 'name': 'Private'})[0]
+    # Create some resources to use in the tests; this was mostly copied from a
+    # test in another repo and has more items and folders than needed
+    collection = Collection().createCollection(
+        'collection A', admin)
+    colFolderA = Folder().createFolder(
+        collection, 'folder A', parentType='collection',
+        creator=admin)
+    colFolderB = Folder().createFolder(
+        collection, 'folder B', parentType='collection',
+        creator=admin)
+    colFolderC = Folder().createFolder(
+        colFolderA, 'folder C', creator=admin)
+    Item().createItem('item A1', admin, colFolderA)
+    Item().createItem('item B1', admin, colFolderB)
+    Item().createItem('item B2', admin, colFolderB)
+    Item().createItem('item C1', admin, colFolderC)
+    Item().createItem('item C2', admin, colFolderC)
+    Item().createItem('item C3', admin, colFolderC)
+    Item().createItem('item Public 1', admin, publicFolder)
+    Item().createItem('item Private 1', admin, privateFolder)
+    folderD = Folder().createFolder(publicFolder, 'folder D', creator=admin)
+    Item().createItem('item D1', admin, folderD)
+    Item().createItem('item D2', admin, folderD)
+    # Now test
+    resp = server.request('/slicer_cli_web/path_match', params={
+        'type': 'folder',
+        'name': '^fold.*D',
+    }, user=admin)
+    assertStatusOk(resp)
+    assert resp.json['name'] == 'folder D'
+
+    resp = server.request('/slicer_cli_web/path_match', params={
+        'type': 'item',
+        'name': '^item',
+        'path': '^/collection/.*/folder.*/folder',
+    }, user=admin)
+    assertStatusOk(resp)
+    assert resp.json['name'] == 'item C3'
+
+    resp = server.request('/slicer_cli_web/path_match', params={
+        'type': 'item',
+        'name': '^item .* 1',
+    }, user=admin)
+    assertStatusOk(resp)
+    assert resp.json['name'] == 'item Private 1'
+
+    resp = server.request('/slicer_cli_web/path_match', params={
+        'type': 'item',
+        'name': '^item .* 1',
+    })
+    assertStatusOk(resp)
+    assert resp.json['name'] == 'item Public 1'
+
+    resp = server.request('/slicer_cli_web/path_match', params={
+        'type': 'item',
+        'name': 'nosuchitem',
+    })
+    assertStatus(resp, 400)
