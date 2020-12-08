54ae37b6919837a45f67fe6010f31741394ec847
David Manthey <david.manthey@kitware.com>
2020-12-08 13:02:10 -0500

    Endpoint support for batch processing.
    See the README for details.

---
+++ slicer_cli_web/web_client/JobStatus.js
@@ -0,0 +1,10 @@
+import JobStatus from '@girder/jobs/JobStatus';
+
+const jobPluginIsCancelable = JobStatus.isCancelable;
+JobStatus.isCancelable = function (job) {
+    if (job.get('type').startsWith('slicer_cli_web_batch')) {
+        return ![JobStatus.CANCELED, JobStatus.WORKER_CANCELING,
+            JobStatus.SUCCESS, JobStatus.ERROR].includes(job.get('status'));
+    }
+    return jobPluginIsCancelable(job);
+};
