7e5ede27b7023ceb2bc74fef7b7198d6a4bc6c41
David Manthey <david.manthey@kitware.com>
2022-11-10 12:16:47 -0500

    Add worker management tools.

--- slicer_cli_web/config.py
+++ slicer_cli_web/config.py
@@ -18,6 +18,7 @@ from girder.api.rest import getCurrentUser
 from girder.constants import AccessType
 from girder.exceptions import ValidationException
 from girder.models.folder import Folder
+from girder.models.item import Item
 from girder.models.setting import Setting
 from girder.settings import SettingDefault
 from girder.utility import setting_utilities
@@ -26,6 +27,7 @@ from girder.utility import setting_utilities
 # Constants representing the setting keys for this plugin
 class PluginSettings:
     SLICER_CLI_WEB_TASK_FOLDER = 'slicer_cli_web.task_folder'
+    SLICER_CLI_WEB_WORKER_CONFIG_ITEM = 'slicer_cli_web.worker_config_item'
 
     @staticmethod
     def has_task_folder():
@@ -37,6 +39,17 @@ class PluginSettings:
         if not folder:
             return None
         return Folder().load(folder, level=AccessType.READ, user=getCurrentUser())
+
+    @staticmethod
+    def has_worker_config_item():
+        return Setting().get(PluginSettings.SLICER_CLI_WEB_WORKER_CONFIG_ITEM) is not None
+
+    @staticmethod
+    def get_worker_config_item():
+        item = Setting().get(PluginSettings.SLICER_CLI_WEB_WORKER_CONFIG_ITEM)
+        if not item:
+            return None
+        return Item().load(item, force=True, exc=False)
 
 
 @setting_utilities.validator({
@@ -52,10 +65,24 @@ def validateFolder(doc):
         raise ValidationException('invalid folder selected')
 
 
+@setting_utilities.validator({
+    PluginSettings.SLICER_CLI_WEB_WORKER_CONFIG_ITEM
+})
+def validateItem(doc):
+    if not doc['value']:
+        return
+    # We should only be able to change settings with admin privilege, so we
+    # don't need to check folder access here.
+    item = Item().load(doc['value'], force=True)
+    if not item:
+        raise ValidationException('invalid folder selected')
+
+
 # Defaults
 
 # Defaults that have fixed values can just be added to the system defaults
 # dictionary.
 SettingDefault.defaults.update({
-    PluginSettings.SLICER_CLI_WEB_TASK_FOLDER: None
+    PluginSettings.SLICER_CLI_WEB_TASK_FOLDER: None,
+    PluginSettings.SLICER_CLI_WEB_WORKER_CONFIG_ITEM: None,
 })
